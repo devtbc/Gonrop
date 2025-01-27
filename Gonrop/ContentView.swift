10e91ff866fa8b46186ac59e973032636bf3dea8
Tyler Stiffler <tyler@greentbc.com>
2025-01-27 12:07:40 -0500

    initial commit. I've got some rounded rects.

--- Gonrop/ContentView.swift
+++ Gonrop/ContentView.swift
@@ -9,13 +9,121 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            VStack {
+                Rectangle()
+                    .foregroundColor(Color.orange)
+                    .frame(width: 1000, height: 300)
+                    .clipShape(UnevenRoundedRectangle(bottomLeadingRadius: 50))
+                    .mask(alphaMaskTop(width:1000, height:300))
+                Spacer()
+                Rectangle()
+                    .foregroundColor(Color.orange)
+                    .frame(width: 1000, height: 600)
+                    .clipShape(UnevenRoundedRectangle(topLeadingRadius: 50))
+                    .mask(alphaMaskBottom(width:1000, height:600))
+            }
+            
+            .padding()
+            .statusBar(hidden: true)
+            .containerRelativeFrame([.horizontal, .vertical])
+            //.ignoresSafeArea()
+            
+            // .mask(maskView())
+            
+            
+            .background(Color.black)
+          
+   
+            
+            
+            
+            
         }
-        .padding()
+    }
+        
+}
+
+@ViewBuilder func alphaMaskTop(width:CGFloat, height:CGFloat) -> some View {
+    //var body: some View {
+        //white is to keep and black is toss
+    VStack(spacing: -height ){
+            Rectangle()
+               // .fill()
+            
+                .foregroundColor(.white)
+                .frame(width: width, height: height)
+        
+//                .mask(alignment: .topLeading){
+//                    UnevenRoundedRectangle(topLeadingRadius: 50)
+//                }
+            Rectangle()
+               // .fill()
+                .foregroundColor(.black)
+                .frame(width: width, height: height)
+                .clipShape(.rect(cornerRadius: 30))
+                .offset(x: 100, y:-30)
+                .overlay(
+                    Text("9:42")
+                        .font(Font.system(size: 90))
+                        .colorInvert()
+                )
+       // }
+        
+    }
+        .compositingGroup()
+        .luminanceToAlpha()
+   // return body
+}
+
+@ViewBuilder func alphaMaskBottom(width:CGFloat, height:CGFloat) -> some View {
+    //var body: some View {
+        //white is to keep and black is toss
+    VStack(spacing: -height ){
+            Rectangle()
+               // .fill()
+                .foregroundColor(.white)
+                .frame(width: width, height: height)
+//                .mask(alignment: .topLeading){
+//                    UnevenRoundedRectangle(topLeadingRadius: 50)
+//                }
+            Rectangle()
+               // .fill()
+                .foregroundColor(.black)
+                .frame(width: width, height: height)
+                .clipShape(.rect(cornerRadius: 30))
+                .offset(x: 100, y:30)
+       // }
+        
+    }
+        .compositingGroup()
+        .luminanceToAlpha()
+   // return body
+}
+
+
+struct maskView: View {
+    var body: some View {
+        //white is to keep and black is toss
+        VStack(spacing: -200 ){
+            Rectangle()
+                .fill()
+                .foregroundColor(.white)
+                .frame(width: nil, height: 300)
+//                .mask(alignment: .topLeading){
+//                    UnevenRoundedRectangle(topLeadingRadius: 50)
+//                }
+            Rectangle()
+                .fill()
+                .foregroundColor(.black)
+                .frame(width: nil, height: 200)
+                .clipShape(.rect(cornerRadius: 30))
+                .offset(x: 50, y:30)
+        }
+        .compositingGroup()
+        .luminanceToAlpha()
     }
 }
 
