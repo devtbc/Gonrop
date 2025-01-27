7a0ecc85d32ab2de0fcf8e4b3ae2859e86391e27
Tyler Stiffler <tyler@greentbc.com>
2025-01-27 08:42:57 -0500

    Initial Commit

---
+++ Gonrop/GonropApp.swift
@@ -0,0 +1,17 @@
+//
+//  GonropApp.swift
+//  Gonrop
+//
+//  Created by Tyler Stiffler on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct GonropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GonropTests/GonropTests.swift
@@ -0,0 +1,17 @@
+//
+//  GonropTests.swift
+//  GonropTests
+//
+//  Created by Tyler Stiffler on 1/27/25.
+//
+
+import Testing
+@testable import Gonrop
+
+struct GonropTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
