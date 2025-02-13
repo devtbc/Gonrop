1940b6ce3794ed12cfa838b6b8f56027b616a3c1
Tyler Stiffler <tyler@greentbc.com>
2025-02-13 15:30:04 -0500

    Updated things conform to view rather than using @viewbuilder. Some clean up and start on active timer

--- Gonrop/ContentView.swift
+++ Gonrop/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
+        
+        
         ZStack{
             
             VStack {
@@ -105,162 +107,176 @@ func todayString() -> String {
     return dayOfTheWeekString
 }
 
-@ViewBuilder func calView() -> some View {
-
+struct calView: View {
     
-    HStack{
-       
-        
-        ForEach(EachWeekDay.allCases, id: \.rawValue) { item in
+    var body: some View{
+        HStack{
             
-            if item.rawValue == todayString(){
-                Rectangle()
-                    .frame(width: 50, height: 120, alignment: .bottom)
-                    .foregroundColor(Color.green)
-                    .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
-                    .overlay(alignment: .top, content: {Text(item.rawValue).padding(.top, 5)})
+            
+            ForEach(EachWeekDay.allCases, id: \.rawValue) { item in
+                
+                if item.rawValue == todayString(){
+                    Rectangle()
+                        .frame(width: 50, height: 120, alignment: .bottom)
+                        .foregroundColor(Color.green)
+                        .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
+                        .overlay(alignment: .top, content: {Text(item.rawValue).padding(.top, 5)})
+                }
+                else{
+                    Rectangle()
+                        .frame(width: 50, height: 100, alignment: .bottom)
+                        .foregroundColor(Color.green)
+                        .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
+                        .overlay(alignment: .top, content: {Text(item.rawValue).padding(.top, 5)})
+                    // .overlay(Text(item.rawValue))
+                }
             }
-            else{
-                Rectangle()
-                    .frame(width: 50, height: 100, alignment: .bottom)
-                    .foregroundColor(Color.green)
-                    .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
-                    .overlay(alignment: .top, content: {Text(item.rawValue).padding(.top, 5)})
-                   // .overlay(Text(item.rawValue))
-            }
+            
+            
+         
         }
-        
-        
-//        EachWeekDay.allCases.forEach { day in
-//            Rectangle()
-//                .frame(width: 50, height: 100, alignment: .bottom)
-//                .foregroundColor(Color.green)
-//                .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
-//                .overlay(Text(day.rawValue))
-//            
-//        }
-       
-        
-//        Rectangle()
-//                        .frame(width: 50, height: 100, alignment: .bottom)
-//                        .foregroundColor(Color.green)
-//                        .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
-//                        .overlay(Text(EachWeekDay.Mon.rawValue))
-//        EachWeekDay.allCases.forEach { day in
-//            print(day.rawValue)
-//        }
-        
-//        ForEach(0..<7){num in
-//            Rectangle()
-//                .frame(width: 50, height: 100, alignment: .bottom)
-//                .foregroundColor(Color.green)
-//                .clipShape(UnevenRoundedRectangle(topLeadingRadius: 10, topTrailingRadius: 10))
-//                .overlay(Text(EachWeekDay[num].rawValue))
-//        }
     }
 }
 
-@ViewBuilder func buttonView() -> some View {
-    Grid{
-        
-        GridRow{
-            GNToggle({}).frame(width: 400)
-                .padding(10)
-                .foregroundStyle(.black)
-                .background(Color.orange)
-                .clipShape(.capsule)
+struct buttonView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    var body: some View{
+        Grid{
             
+            GridRow{
+                GNToggle("Unfinshed Toggle!", {}).frame(width: 400)
+                    .padding(10)
+                    .foregroundStyle(.black)
+                    .background(Color.orange)
+                    .clipShape(.capsule)
+                
+                timerView()//.onReceive(<#T##publisher: Publisher##Publisher#>, perform: <#T##(Publisher.Output) -> Void#>)
+                //.onReceive(timer){$timeRemaining -= 1}
+                //.onReceive(timer)
+                //                .onReceive(timer){
+                //                    body
+                //                }
+            }
             
-        }
-        
-        GridRow {
-            ForEach(0..<2) { _ in GNButton({}) }
-        }
-        .gridColumnAlignment(.leading)
-        .padding(.vertical, 10)
-        
-        GridRow{
-            
-//            Button(action: {}) {
-//                Text("Hello, Bttn!")
-//                    .font(Font.system(size: 90))
-//            }
-//            .padding(10)
-//            .foregroundStyle(.black)
-//            .background(Color.orange)
-//            .clipShape(.capsule)
-        }
-        
-        GridRow{
-            Button(action: {}) {
-                Text("Hello, Button!")
-                    .font(Font.system(size: 40))
-            }.frame(width: 400)
-                .padding(10)
-                .foregroundStyle(.black)
-                .background(Color.orange)
-                .clipShape(.capsule)
-        }.gridColumnAlignment(.leading)
+            GridRow {
+                ForEach(0..<2) { num in GNButton("This is \(num) Button!", {}) }
+            }
+            .gridColumnAlignment(.leading)
             .padding(.vertical, 10)
+            
+            GridRow{
+                
+                //            Button(action: {}) {
+                //                Text("Hello, Bttn!")
+                //                    .font(Font.system(size: 90))
+                //            }
+                //            .padding(10)
+                //            .foregroundStyle(.black)
+                //            .background(Color.orange)
+                //            .clipShape(.capsule)
+            }
+            
+            GridRow{
+                Button(action: {}) {
+                    Text("Hello, Button!")
+                        .font(Font.system(size: 40))
+                }.frame(width: 400)
+                    .padding(10)
+                    .foregroundStyle(.black)
+                    .background(Color.orange)
+                    .clipShape(.capsule)
+            }.gridColumnAlignment(.leading)
+                .padding(.vertical, 10)
+            
+        }  //end of grid
         
-    }  //end of grid
-    //.frame(width: 500, height: 100, alignment: .bottom)
-    //.frame(alignment: .bottom)//
+    }
+}
+
+struct timerView: View {
     
+    var timeRemaining = 240
+//    public func goDown() -> Void{
+//        $timeRemaining -= 1
+//    }
+    
+    var body: some View{
+        //Date.init(timeIntervalSinceNow: 240)
+        
+        // public func hmsFrom() -> (Int, Int, Int) {
+        //    return (self / 3600, (self % 3600) / 60, (self % 3600) % 60)
+        
+        Text("\((timeRemaining % 3600) / 60): \((timeRemaining % 3600) % 60)")
+        
+        //    Text(timeRemaining.formatted(Date.FormatStyle()
+        //        .day(.omitted)
+        //        .month(.omitted)
+        //        .year(.omitted)
+        //        .hour(.twoDigits(amPM: .omitted)).second()
+        //    ))
+            .font(Font.system(size: 60))
+        // .colorInvert() format: .dateTime.hour(.twoDigits(amPM: .omitted)).minute().second()
+            .monospaced()
+            .foregroundColor(.orange)
+    }
+    }
+
+struct clockView: View {
+    var body: some View{
+        Text(Date.now, format: .dateTime.hour(.twoDigits(amPM: .omitted)).minute().second())
+            .font(Font.system(size: 90))
+            .colorInvert()
+            .monospaced()
+    }
 }
 
-@ViewBuilder func clockView() -> some View {
-    Text(Date.now, format: .dateTime.hour(.twoDigits(amPM: .omitted)).minute().second())
-        .font(Font.system(size: 90))
-        .colorInvert()
-        .monospaced()
-}
-
-@ViewBuilder func alphaMaskTop(width:CGFloat, height:CGFloat) -> some View {
-    //var body: some View {
-        //white is to keep and black is toss
-    VStack(spacing: -height ){
+struct alphaMaskTop: View {
+    let width:CGFloat, height:CGFloat
+    //white is to keep and black is toss
+    var body: some View{
+        VStack(spacing: -height ){
             Rectangle()
                 .foregroundColor(.white)
                 .frame(width: width, height: height)
-        
+            
             Rectangle()
                 .foregroundColor(.black)
                 .frame(width: width, height: height)
                 .clipShape(.rect(cornerRadius: 30))
                 .offset(x: 100, y:-30)
-                
-
-        
-    }
+            
+            
+            
+        }
         .compositingGroup()
         .luminanceToAlpha()
-   // return body
+    }
 }
 
-@ViewBuilder func alphaMaskBottom(width:CGFloat, height:CGFloat) -> some View {
-    //var body: some View {
+struct alphaMaskBottom :View {
+    let width:CGFloat, height:CGFloat
+    var body: some View {
         //white is to keep and black is toss
-    VStack(spacing: -height ){
+        VStack(spacing: -height ){
             Rectangle()
-               // .fill()
+            // .fill()
                 .foregroundColor(.white)
                 .frame(width: width, height: height)
-//                .mask(alignment: .topLeading){
-//                    UnevenRoundedRectangle(topLeadingRadius: 50)
-//                }
+            //                .mask(alignment: .topLeading){
+            //                    UnevenRoundedRectangle(topLeadingRadius: 50)
+            //                }
             Rectangle()
-               // .fill()
+            // .fill()
                 .foregroundColor(.black)
                 .frame(width: width, height: height)
                 .clipShape(.rect(cornerRadius: 30))
                 .offset(x: 100, y:30)
-       // }
-        
-    }
+            // }
+            
+        }
         .compositingGroup()
         .luminanceToAlpha()
-   // return body
+    }
 }
 
 
--- Gonrop/GNButtons.swift
+++ Gonrop/GNButtons.swift
@@ -7,23 +7,16 @@
 
 import SwiftUI
 
-func GNButton(_ action: @escaping() -> Void) -> some View {
-    return Button(action: {}) {
-        Text("This is__<>Button!")
-            .font(  .system( size: 28)
-                .weight(.semibold)
-                    .monospaced()
-                    
-            )
-        
-           
+func GNButton(_ label: String, _ action: @escaping() -> Void) -> some View {
+    return Button(label) { action()
     }
     
-.frame(width: 400)
-    .padding(10)
-    .foregroundStyle(.black)
-    .background(Color.orange)
-    .clipShape(.capsule)
+    .buttonStyle(GNButtonStyle())
+//.frame(width: 400)
+//    .padding(10)
+//    .foregroundStyle(.black)
+//    .background(Color.orange)
+//    .clipShape(.capsule)
     
 //    .padding(.horizontal, 30)
 //    //.padding(20)
@@ -35,13 +28,13 @@ func GNButton(_ action: @escaping() -> Void) -> some View {
 
 
 
-func GNToggle(_ action: @escaping() -> Void) -> some View {
+func GNToggle(_ label: String, _ action: @escaping() -> Void) -> some View {
     @State var toggleState = false
 
 
     var tmpBody: some View {
         Toggle(isOn: $toggleState) {
-            Text("Unfinshed Toggle!")
+            Text(label)
                 .font(  .system( size: 28)
                     .weight(.semibold)
                         .monospaced()
@@ -57,8 +50,37 @@ func GNToggle(_ action: @escaping() -> Void) -> some View {
 
 }
 
-
-
-#Preview {
-    GNButton({})
+struct GNButtonStyle: ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(  .system( size: 28)
+                .weight(.semibold)
+                    .monospaced()
+                    )
+            .frame(width: 400)
+            .padding(10)
+            .foregroundStyle(.black)
+            .background(Color.orange)
+            .clipShape(.capsule)
+    }
 }
+
+//struct CustomToggleStyle: ToggleStyle {
+//    func makeBody(configuration: Configuration) -> some View {
+//        Button(action: {
+//            configuration.isOn.toggle()
+//        }) {
+//            configuration.label
+//                .padding()
+//                .background(configuration.isOn ? Color.green : Color.red)
+//                .foregroundColor(.white)
+//                .cornerRadius(8)
+//        }
+//    }
+//}
+
+
+
+//#Preview {
+//    GNButton({})
+//}
